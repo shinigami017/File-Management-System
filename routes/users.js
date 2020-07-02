d65ee2473b5abc1c09f4cc4830aba302abd1953d
shinigami017 <satyambuddy77@gmail.com>
2020-07-02 14:46:30 +0530

    Updated change password feature and added nodemon

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,10 @@
-var express = require("express"),
+const express = require("express"),
     router = express.Router(),
     bcrypt = require("bcryptjs"),
     passport = require("passport");
-
 // Load User Model
-var User = require("../models/user");
-var { isLoggedIn, forwardAuthenticated } = require('../config/auth');
+const User = require("../models/user");
+const { isLoggedIn, forwardAuthenticated } = require('../config/auth');
 
 // Register Form
 router.get("/register", forwardAuthenticated, function(request, response) {
@@ -19,9 +18,12 @@ router.get("/login", forwardAuthenticated, function(request, response) {
 
 // Register User
 router.post("/register", function(request, response) {
-    var { username, firstname, lastname, email, phone, role, password, password2 } = request.body;
+
+    // get user details from request
+    const { username, firstname, lastname, email, phone, role, password, password2 } = request.body;
     let errors = [];
 
+    // form validations
     if (!username) {
         errors.push({ msg: "Please enter username" });
     }
@@ -46,16 +48,16 @@ router.post("/register", function(request, response) {
         errors.push({ msg: "Please enter password" });
     }
 
+    if (password.length < 6) {
+        errors.push({ msg: "Password must be at least 6 character long" });
+    }
+
     if (!password2) {
         errors.push({ msg: "Please confirm password" });
     }
 
     if (password != password2) {
         errors.push({ msg: "Passwords do not match" });
-    }
-
-    if (password.length < 6) {
-        errors.push({ msg: "Password must be at least 6 character long" });
     }
 
     if (errors.length > 0) {
@@ -74,7 +76,7 @@ router.post("/register", function(request, response) {
                 // if (role == "student") {
                 //     newRole = new Student({ username: username, name: name, email: email, phone: phone, password: password });
                 // }
-                var newUser = new User({ username, firstname, lastname, email, phone, role, password });
+                let newUser = new User({ username, firstname, lastname, email, phone, role, password });
                 bcrypt.genSalt(10, function(error, salt) {
                     bcrypt.hash(newUser.password, salt, function(error, hash) {
                         if (error) throw error;
@@ -160,31 +162,82 @@ router.post("/editprofile", isLoggedIn, function(request, response) {
 
 // Get the user's change password page
 router.get("/changepassword", isLoggedIn, function(request, response) {
-    // get user and send it to change password
-    // Student.findOne({ username: request.user.username }, function(error, foundStudent) {
-    //     if (error) {
-    //         response.json(error);
-    //     } else {
-    //         response.render("files/change-password", { currentUser: foundStudent });
-    //     }
-    // });
-    User.findOne({ username: request.user.username }, function(error, foundUser) {
-        if (error) {
-            response.json(error);
-        } else {
-            response.render("files/student-portal/change-password", { currentUser: foundUser });
-        }
-    });
+    response.render("files/change-password", { currentUser: request.user });
 });
 
 // Post method to process the password change
 router.post("/changepassword", isLoggedIn, function(request, response) {
-    // get user details from body and update it to database
-    // if error generated then redirect to changepassword with the error message
-    // else redirect to login with successful password change message
-    response.redirect("/fms.edu.in/users/login");
-});
 
+    // get user details from request
+    const { username, cpassword, password, password2 } = request.body;
+    let errors = [];
+
+    // form validations
+    if (!cpassword) {
+        errors.push({ msg: "Please enter your current password" });
+    }
+
+    if (!password) {
+        errors.push({ msg: "Please enter new password" });
+    }
+
+    if (password.length < 6) {
+        errors.push({ msg: "Password must be at least 6 character long" });
+    }
+
+    if (!password2) {
+        errors.push({ msg: "Please confirm new password" });
+    }
+
+    if (password != password2) {
+        errors.push({ msg: "Passwords do not match" });
+    }
+    // match current password with the one entered by user
+    bcrypt.compare(cpassword, request.user.password, function(error, isMatch) {
+        if (error) {
+            console.log(error);
+            request.flash("error_msg", "Something went wrong. Please try again");
+            return response.redirect("/fms.edu.in/users/changepassword");
+        } else if (!isMatch) {
+            errors.push({ msg: "Please enter your valid current password" });
+            return response.render("files/change-password", { currentUser: request.user, errors });
+        } else {
+            // if error generated then redirect to changepassword with the error message
+            if (errors.length > 0) {
+                return response.render("files/change-password", { currentUser: request.user, errors });
+            }
+            // else update the new password in database
+            User.findOne({ username: username }, function(error, foundUser) {
+                if (error) {
+                    console.log(error);
+                    request.flash("error_msg", "Something went wrong. Please try again");
+                    return response.redirect("/fms.edu.in/users/changepassword");
+                }
+                bcrypt.genSalt(10, function(error, salt) {
+                    bcrypt.hash(password, salt, function(error, hash) {
+                        if (error) {
+                            console.log(error);
+                            request.flash("error_msg", "Something went wrong. Please try again");
+                            return response.redirect("/fms.edu.in/users/changepassword");
+                        }
+                        foundUser.password = hash;
+                        foundUser.save(function(error, user) {
+                            if (error) {
+                                console.log(error);
+                                request.flash("error_msg", "Something went wrong. Please try again");
+                                return response.redirect("/fms.edu.in/users/changepassword");
+                            }
+                            console.log("User password changed.")
+                            request.logout();
+                            request.flash("success_msg", "Password changed. Log in to continue");
+                            return response.redirect("/fms.edu.in/users/login");
+                        });
+                    });
+                });
+            });
+        }
+    });
+});
 
 // Logout User
 router.get("/logout", function(request, response) {
@@ -192,9 +245,5 @@ router.get("/logout", function(request, response) {
     request.flash("success_msg", "You're successfully logged out");
     response.redirect("/fms.edu.in/users/login");
 });
-// router.get("/logout", function(request, response) {
-//     request.logout();
-//     response.redirect("/fms.edu.in");
-// });
 
 module.exports = router;
